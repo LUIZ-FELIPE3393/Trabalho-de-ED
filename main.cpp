68346a6db24c78784130b9089dff144ecfb4ea5f
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2023-08-16 22:05:41 -0300

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   main.cpp 	new file:   main.exe

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "--------------\n";
+    cout << " OO  L     AA\n";
+    cout << "O  O L    A  A\n";
+    cout << "O  O L    AAAA\n";
+    cout << "O  O L    A  A\n";
+    cout << "O  O L    A  A\n";
+    cout << " OO  LLLL A  A\n";
+    cout << "--------------\n";
+}
