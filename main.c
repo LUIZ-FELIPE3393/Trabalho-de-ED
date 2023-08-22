538126bbd45e773e2cdc5ce1827e6e5c22ada3ba
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2023-08-22 15:35:52 -0300

    main.cpp -> main.c

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define MAX_BUFFER_SIZE 128
+
+FILE* arquivo = NULL;
+
+//Funções Leitura de Arquivos (Declarar)
+bool abrirArquivo(const char* caminho);
+int consulta(int id);
+
+//Registros (Estruturas)
+
+//Funções (Features do programa)
+
+
+int main()
+{
+    printf("Olá Mundo\n");
+
+    if (!abrirArquivo("registros.txt"))
+    {
+        return -1;
+    }
+
+    char BUFFER [MAX_BUFFER_SIZE];
+
+    consulta(0);
+
+    return 0;
+}
+
+//Funções Leitura de Arquivos (Implementar)
+bool abrirArquivo(const char* caminho)
+{
+    if((arquivo = fopen(caminho, "r")) == NULL)
+    {
+        printf("Não foi possível abrir o arquivo \"%s\" \n", caminho);
+        return false;
+    }
+
+    return true;
+}
+
+int consulta(int id)
+{
+    char c;
+    while (c = getc(arquivo) != EOF)
+    {
+        printf("%c", c);
+    }
+    return c;
+}
