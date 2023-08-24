52d21ec4eb5c1e64e836684996c3021f4907601d
LUIZ-FELIPE3393 <142448247+LUIZ-FELIPE3393@users.noreply.github.com>
2023-08-24 11:59:58 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,3 +1,57 @@
+<<<<<<< HEAD
+#include <stdio.h>
+#include <stdbool.h>
+#define MAX_BUFFER_SIZE 128
+
+FILE* arquivo = NULL;
+
+//Funções Leitura de Arquivos (Declarar)
+bool abrirArquivo(const char* caminho);
+int consulta(int id);
+
+//Registros (Estruturas)
+
+//Funções (Features do programa)
+
+
+int main()
+{
+    printf("Olá Mundo\n");
+
+    if (!abrirArquivo("registros2.txt"))
+    {
+        return -1;
+    }
+
+    char BUFFER [MAX_BUFFER_SIZE];
+
+    consulta(0);
+
+    return 0;
+}
+
+//Funções Leitura de Arquivos (Implementar)
+bool abrirArquivo(const char* caminho)
+{
+    if((arquivo = fopen(caminho, "w")) == NULL)
+    {
+        printf("Não foi possível abrir o arquivo \"%s\" \n", caminho);
+        return false;
+    }
+
+    return true;
+}
+
+int consulta(int id)
+{
+    char c;
+    while (c = getc(arquivo) != EOF)
+    {
+        printf("%c", c);
+    }
+    return c;
+}
+=======
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -52,3 +106,4 @@ int consulta(int id)
     }
     return c;
 }
+>>>>>>> 40d5f05260f64eb75bb67e3fb7e547fc807205db
