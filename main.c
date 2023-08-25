be8b31369fc89ea381384468164da1e77ce5a59b
LUIZ-FELIPE3393 <142448247+LUIZ-FELIPE3393@users.noreply.github.com>
2023-08-24 22:44:12 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include <locale.h>
-#define MAX_FILE_SIZE 128
+#define MAX_FILE_SIZE 4096
 
 FILE* arquivo = NULL;
+char BUFFER [MAX_FILE_SIZE];
 
 //FunÃ§Ãµes Leitura de Arquivos (Declarar)
 bool abrirArquivo(const char* caminho);
@@ -12,7 +14,8 @@ bool abrirArquivo(const char* caminho);
 void registrarPessoa(); // FUNCAO TESTE
 
 //EnumeraÃ§Ãµes
-typedef enum { PADEIRO, MEDICO, DENTISTA, SAPATEIRO } Profissao; // ENUM TESTE
+typedef enum { PR_NULL, PADEIRO, MEDICO, DENTISTA, SAPATEIRO } Profissao; // ENUM TESTE
+Profissao retornaProfPorNome(const char* nome_prof);
 
 //Registros (Estruturas)
 typedef struct  // REGISTRO TESTE
@@ -42,6 +45,10 @@ int main()
     }
 
 	lerRegistroPessoa(&p1, 0);
+	
+	printf("X_NOME: %s\n", p1.nome);
+	printf("X_IDADE: %d\n", p1.idade);
+	printf("X_PROF: %d\n", p1.prof);
 
     return 0;
 }
@@ -59,52 +66,137 @@ bool abrirArquivo(const char* caminho)
     
 }
 
+Profissao retornaProfPorNome(const char* nome_prof)
+{
+	if (strcmp(nome_prof, "MEDI") == 0)
+	{
+		return MEDICO;
+	}
+	else if (strcmp(nome_prof, "PADE") == 0)
+	{
+		return PADEIRO;
+	}
+	else if (strcmp(nome_prof, "SAPA") == 0)
+	{
+		return SAPATEIRO;
+	}
+	else if (strcmp(nome_prof, "DENT") == 0)
+	{
+		return DENTISTA;
+	}
+	else 
+	{
+		return PR_NULL;
+	}
+}
+
 void lerRegistroPessoa(Pessoa* p, int id_registro)
 {
-	char BUFFER [MAX_BUFFER_SIZE];
-	char *sub_BUFFER, num_BUFFER[16];
+	char line_BUFFER[128], sub_BUFFER[16];
     char c;
-    bool lerRegistro;
-    int i = 0, ii = 0;
+    bool lerRegistro, linha_completa;
+    int i = 0, ii = 0, fase = 0;
     int id_atual = 0;
+    
+    //Leitura de Arquivo
     while ((c = fgetc(arquivo)) != EOF)
     {
-    	BUFFER[i++] = c;   
-    }
-    BUFFER[i] = '\0';
-    
-    sub_BUFFER = BUFFER;
-    
-    i = 0;
-    while ((c = sub_BUFFER[i++] )!= '\0')
-    {
-		if (lerRegistro)
+	    if (c != '\n')
+	    {
+	    	line_BUFFER[i++] = c;  	
+	    } 
+		else 
 		{
-			if (sub_BUFFER[i] == '(')
-			{
-				while (sub_BUFFER[++i] != ')')
-			}
+			linha_completa = true; 
+			line_BUFFER[i] = '\0';
 		}
-    	
-    	if (isdigit(c))
-    	{
-    		num_BUFFER[ii++] = c;
-			printf("c:%c\n", c);	
-		}
-		else
-		{
-			id_atual = atoi(num_BUFFER);
-			printf("id_atual:%d\n", id_atual);
-			if(id_atual == id_registro)	
+	    
+	    printf("sub_BUFFER:%s\n", line_BUFFER);
+	    
+	    if(!linha_completa)
+	    	continue;
+	    i = 0;
+	    //Leitura de Registro
+	    while ((c = line_BUFFER[i++] )!= '\0') 
+	    {
+			if (lerRegistro)
 			{
-				lerRegistro = true;
+				printf("LerRegistro\n");
+				if (line_BUFFER[i] == '(')
+				{
+					while (line_BUFFER[++i] != ')')
+					{
+						//Dentro do Parênteses
+						printf("Dentro\n");
+						ii = 0;
+						do
+						{
+							//NOME
+							p->nome[ii++] = line_BUFFER[i];
+							i++;
+						} while (line_BUFFER[i] != ',');
+						
+						p->nome[ii] = '\0';
+						printf("NOME: %s\n", p->nome);
+						memset(sub_BUFFER, 0, sizeof(sub_BUFFER));
+						
+						ii = 0;
+						while ( (c = line_BUFFER[++i] ) != ',')
+						{
+							//IDADE
+							if (isdigit(c))
+						   	{
+						   		sub_BUFFER[ii++] = c;
+								printf("c:%c\n", c);	
+							}
+						}
+						
+						printf("idade BUFFER:%s\n", sub_BUFFER);
+						p->idade = atoi(sub_BUFFER);
+						
+						ii = 0;
+						while ((c = line_BUFFER[++i]) != ')') // Termina Leitura
+						{
+							printf("PROF: ");
+							//PROFISSÃO
+							if(!isspace(c))
+							{
+								sub_BUFFER[ii++] = c;
+							}
+							
+							printf("sub_buffer i:%c\n", line_BUFFER[i]);
+						}
+						
+						p->prof = retornaProfPorNome(sub_BUFFER);
+						lerRegistro = false;
+						return;
+					}
+				}
 			}
-		}	
+	    	
+	    	if (isdigit(c)) // Converte id do registro para inteiro
+	    	{
+	    		sub_BUFFER[ii++] = c;
+				printf("c:%c\n", c);	
+			}
+			else
+			{
+				// Verifica se é a id correta
+				id_atual = atoi(sub_BUFFER);
+				printf("id_atual:%d\n", id_atual);
+				if(id_atual == id_registro)	
+				{
+					printf("id_atual:%d == id:%d\n", id_atual, id_registro);
+					lerRegistro = true;
+				}
+			}	
+		}
+		memset(line_BUFFER, 0, sizeof(line_BUFFER));
 	}
 	
-	printf("num_BUFFER:%s\n", num_BUFFER);
+	printf("num_BUFFER:%s\n", sub_BUFFER);
     
-    printf("%s\n", BUFFER);
+    //printf("%s\n", BUFFER);
 }
 
 
